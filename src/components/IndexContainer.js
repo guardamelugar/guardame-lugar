fadb71c9055a07cf1d4faf5f485f2227d6ed65e1
Rodrigo Ruiz <rodrigo.ruiz@mrm-mccann.com>
2020-06-05 21:11:00 -0300

    Añadido contenido clientindex, componente ClienteNavBar, arreglo de cookie validation, route a formulario de registro garage

--- src/components/IndexContainer.js
+++ src/components/IndexContainer.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import Header from './Header'
 import GarageSearch from './GarageSearch'
+import ClienteNavBar from './ClienteNavBar'
 import ChequearCookie from './ChequearCookie'
 
 const IndexContainer = () => {
-  const salida =
+  const salidaUser =
   <section>
     <Header />
     <GarageSearch />
   </section>
-  return (ChequearCookie(salida, '/index', '/clientindex', '/login'));
+
+  const salidaCliente =
+  <section>
+    <Header />
+    <ClienteNavBar />
+  </section>
+  
+  if(window.location.pathname === '/index'){
+    return (ChequearCookie(salidaUser, '/index', '/clientindex', '/login'));
+  }
+  
+  if(window.location.pathname === '/clientindex'){
+    return (ChequearCookie(salidaCliente, '/index', '/clientindex', '/login'));
+  }
 
 }
 
