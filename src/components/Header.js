b774cde9e095b01b0e7ca965e0973d5f15d68adc
Rodrigo Ruiz <rodrigo.ruiz@mrm-mccann.com>
2020-06-28 20:05:58 -0300

    Font change para logo y dimensionado de header

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,6 +27,7 @@ class Header extends Component {
         <Navbar bg="secondary" expand="sm" className="padding-nav" variant="dark">
           <Navbar.Brand href="/">
             <img src={require("../img/logo.png")} className='header-logo' alt="logo"></img>
+            Guardame Lugar
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" bsPrefix="no-margin-top navbar-toggler"/>
           <Navbar.Collapse id="basic-navbar-nav">
