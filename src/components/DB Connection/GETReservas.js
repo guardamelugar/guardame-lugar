260ff65e5733ce5b7435fd14152dfe7ca501415b
gabrielmaceira <gabrielmaceira@gmail.com>
2020-06-28 09:04:02 -0300

    GETReservas feature Añadido GETReservas.js (usuario y cliente) Cambio URL.js urls para reservas por cliente y usuario

---
+++ src/components/DB Connection/GETReservas.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { URL_RESERVAS_POR_GARAGE, URL_RESERVAS_USUARIO } from '../../constants/URL'
+import { trackPromise } from 'react-promise-tracker'
+
+/* reservas por usuario necesita como props
+{rol: "",
+user_id: ""}
+
+reservas por cliente necesita como props
+{rol: "",
+garage_id:""} */
+
+const GETReservas = props => {
+  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+  let url_final = undefined;
+  
+  if (parseInt(props.rol,10) === 1) {
+    url_final = URL_RESERVAS_USUARIO + (parseInt(props.user_id,10));
+  }
+  if (parseInt(props.rol,10) === 2) {
+    url_final = URL_RESERVAS_POR_GARAGE + (parseInt(props.garage_id,10));
+  }
+  
+  return trackPromise(axios.get(url_final)
+    .then(res => {
+      if (res.status === 200) {
+        return res.data.result;
+      }
+    }))
+    .catch(function (err) {
+      if (err.response) {
+        if (err.response.status !== 500) {
+          alert(err.response.data.message);
+        } else {
+          alert("No se puede conectar con la base de datos")
+        }
+      }
+    })
+}
+
+export default GETReservas;
